f6272bfb3c49af863a6d08c685b2c55ebad823e8
paramw20 <97079232+paramw20@users.noreply.github.com>
2023-11-03 18:44:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdC7awhQA-DMcoGnt_6XxoyWTftB0DN8o",
-  authDomain: "insta-c1711.firebaseapp.com",
-  projectId: "insta-c1711",
-  storageBucket: "insta-c1711.appspot.com",
-  messagingSenderId: "392910635344",
-  appId: "1:392910635344:web:e332faf08bc5fdf4d02c55",
-  measurementId: "G-X6FB6WPGSN"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
